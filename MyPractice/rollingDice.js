6afd419547e2cdc0620231e697e33774d445d732
sowmyanagayya <kkd.sowmya@gmail.com>
2022-01-28 18:29:24 -0600

    working on Restuarant created necessary html file and working on homepage

--- MyPractice/rollingDice.js
+++ MyPractice/rollingDice.js
@@ -32,7 +32,7 @@
 var numSides = prompt("How many sides does the die have?");
 
 function rollDice(numSides) {
-    return Math.floor(Math.random() * numSides) + 1;
+    return Math.floor(Math.random() * numSides);
 }
 
 for (var i = 0; i < 100; i++) {
