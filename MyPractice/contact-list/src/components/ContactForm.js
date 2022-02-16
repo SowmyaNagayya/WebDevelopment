723889695d6aaf62ee96b2ff6a870fcae8e9f1eb
sowmyanagayya <kkd.sowmya@gmail.com>
2022-02-16 17:58:19 -0600

    worked on creating the contact form

--- MyPractice/contact-list/src/components/ContactForm.js
+++ MyPractice/contact-list/src/components/ContactForm.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Form, Button } from 'react-bootstrap'
 
-function ContactForm (){
-  return <h1>Contact Form</h1>
+class ContactForm extends React.Component {
+
+    render() {
+        return (
+            <Form>
+                <Form.Group controlId="contactFirstName">
+                    <Form.Label>First Name:</Form.Label>
+                    <Form.Control type="text" placeholder="First Name" name="firstName"/>
+                </Form.Group>
+                <Form.Group controlId="contactLastName">
+                    <Form.Label>Last Name:</Form.Label>
+                    <Form.Control type="text" placeholder="Last Name" name="lastName"/>
+                </Form.Group>
+                <Form.Group controlId="contactCompany">
+                    <Form.Label>Company:</Form.Label>
+                    <Form.Control type="text" placeholder="Company" name="company"/>
+                </Form.Group>
+                <Form.Group controlId="contactPhone">
+                    <Form.Label>Phone Number:</Form.Label>
+                    <Form.Control type="phone" placeholder="Phone Number" name="phone" />
+                </Form.Group>
+                <Form.Group controlId="contactEmail">
+                    <Form.Label>Email:</Form.Label>
+                    <Form.Control type="email" placeholder="Email" name="email"/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        )
+    }
 }
 
 export default ContactForm
